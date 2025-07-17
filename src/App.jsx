c29650df1b333c9efb78d31461d928f7b2bd7793
Menardo Madiumba <Menardotera@gmail.com>
2025-07-17 09:36:47 +0200

    Le Hook useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,30 @@
-import { useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
+
+/**
+ *
+ * Le hoock useRef
+ * Permet d'interargir avec le HTML et de conserver la valeur
+ *
+ */
 
 function App() {
-  /**
-   *
-   * Le useEffect permet de suivre le cycle de vie d'un composant
-   *
-   * Le cycle de vie d'un composant inclus
-   *
-   * A PHASE DE MOUNTING (Montage) Creation d'un composant
-   *    1 Lecture rendue
-   *    2 Lecture useEffect
-   * B PHASE DE UPDATE   (Mise a jour) Evolution d'un Composantp
-   *    1 Lecture re rendue
-   *    2 Lecture useEffect
-   * C PHASE DE UNMOUNTING (Mord) Le demontage d'un composant
-   *    1 *
-   *    2
-   *
-   */
+  const ref = useRef(undefined);
 
-  const [count, setCount] = useState(0);
-
-  console.log("ok");
-
-  const handleClick = () => {
-    /**
-     * Le setter nous permet de passer d'un phase au autre
-     * donc de quitter la phase 1 à la phase 2
-     */
-    setCount(count + 1);
-  };
-
-  /**
-   * Le useEffect se declanche toujour lors de la deuxieme lecture.
-   * Nous avons aussi le moyen de le lancer lors de
-   *
-   * useEffect prend a deuxieme paramettre un tableaux de dependance
-   * un tableau des elements a suivre
-   */
   useEffect(() => {
-    /**
-     * Par contre si nous voulons executer uniquemennt une fois
-     * le useEffect prend en seconde parametre un tableau
-     * [
-     *    si c'est un tableau vide le useEffect va se declancher qu'une seule fois
-     *    si un lui passe une variable il va suivre c'est element
-     * ]
-     */
-    console.log("usEffect");
-  }, [count]);
-
-  console.log("rendu");
+    console.log(ref.current);
+  }, []);
 
   return (
     <>
-      <h1>Le useEffect</h1>
-      <h1>{count}</h1>
-      <button onClick={handleClick}>Incrementer</button>
+      <h1>Lorem ipsum dolor</h1>
+      <Input ref={ref} />
     </>
   );
+}
+
+function Input({ ref }) {
+  console.log(ref);
+  return <input ref={ref} type="text" placeholder="votre champ" />;
 }
 
 export default App;
